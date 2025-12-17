d748b441ba5bfc4ceafbb50ed4685a4ce3ed573f
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-17 12:00:51 +0530

    User Controller

---
+++ Server/controllers/userController.js
@@ -0,0 +1,27 @@
+import User from "../models/UserModel.js";
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        const userId = req.userId;
+        if(!userId) {
+            return res.status(400).json({
+                message: 'UserId not found!'
+            });
+        }
+        
+        const user = await User.findById(userId);
+        if(!user) {
+            return res.status(400).json({
+                message: 'User not found!'
+            });
+        }
+
+        return res.status(200).json(user);
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: `Get-Current-User Error: ${error}`
+        });
+    }
+};
