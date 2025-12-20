24202e3e8beba724d39734aac7ed35123ff122cd
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-20 18:40:30 +0530

    User Route

---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getCurrentUser, updateUserLocation } from '../controllers/userController.js';
+import isAuth from '../middlewares/isAuth.js';
+
+
+const userRouter = express.Router();
+
+
+userRouter.get('/current-user', isAuth, getCurrentUser);
+userRouter.post('/update-location', isAuth, updateUserLocation);
+
+
+export default userRouter;
