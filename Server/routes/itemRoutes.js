791ea2f814881a98a52d66ee6071795030e11c0e
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-21 21:09:26 +0530

    Item Route

--- Server/routes/itemRoutes.js
+++ Server/routes/itemRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import isAuth from '../middlewares/isAuth.js';
-import { addItem, deleteItem, editItem, getItemByCity, getItemById } from '../controllers/itemController.js';
+import { addItem, deleteItem, editItem, getItemByCity, getItemById, getItemsByShop } from '../controllers/itemController.js';
 import upload from '../middlewares/multer.js';
 
 
@@ -11,6 +11,7 @@ itemRouter.post('/edit-item/:itemId', isAuth, upload.single('image'), editItem);
 itemRouter.get('/get-item-by-id/:itemId', isAuth, getItemById);
 itemRouter.get('/delete-item/:itemId', isAuth, deleteItem);
 itemRouter.get('/get-item-by-city/:city', isAuth, getItemByCity);
+itemRouter.get('/get-item-by-shop/:shopId', isAuth, getItemsByShop);
 
 
 export default itemRouter; 
