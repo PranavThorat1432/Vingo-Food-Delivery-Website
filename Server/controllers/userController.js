93c18c7b2ed3e279f5f525d7df4a09a42ee2342d
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-22 18:50:56 +0530

    User Controller

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -25,3 +25,32 @@ export const getCurrentUser = async (req, res) => {
         });
     }
 };
+
+
+export const updateUserLocation = async (req, res) => {
+    try {
+        const {lat, lon} = req.body;
+        const userId = await User.findByIdAndUpdate(req.userId, {
+            location: {
+                type: 'Point',
+                coordinates: [lon, lat]
+            }
+        }, {new: true});
+
+        if(!userId) {
+            return res.status(400).json({
+                message: 'UserId not found!'
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Location Updated!'
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: `Update-User-Location Error: ${error}`
+        });
+    }
+};
