e1c9a010f0e0b7008ca53cb9d0da1153472e9a06
PranavThorat1432 <pranavthorat95@gmail.com>
2025-12-22 18:51:32 +0530

    Order Route

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import isAuth from '../middlewares/isAuth.js';
-import { acceptOrder, getCurrentOrder, getDeliveryAssignment, getMyOrders, getOrderById, placeOrder, sendDeliveryOTP, updateOrderStatus, verifyDeliveryOTP } from '../controllers/OrderController.js';
+import { acceptOrder, getCurrentOrder, getDeliveryAssignment, getMyOrders, getOrderById, placeOrder, sendDeliveryOTP, updateOrderStatus, verifyDeliveryOTP, verifyPayment } from '../controllers/OrderController.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post('/place-order', isAuth, placeOrder);
+orderRouter.post('/verify-payment', isAuth, verifyPayment);
 orderRouter.get('/my-orders', isAuth, getMyOrders);
 orderRouter.post('/update-status/:orderId/:shopId', isAuth, updateOrderStatus);
 orderRouter.get('/get-delivery-assignment', isAuth, getDeliveryAssignment);
